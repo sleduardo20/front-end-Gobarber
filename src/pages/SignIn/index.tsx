2822a1e8420833a4b6feb9520f452c7d77ad93b8
sleduardo20 <sl.eduardo20@gmail.com>
2020-09-24 22:28:48 -0300

    chore: updated page SingIn with validations

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
@@ -8,12 +12,36 @@ import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha Obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      const errors = getValidationErrors(error);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
           <Input
@@ -22,9 +50,9 @@ const SignIn: React.FC = () => {
             type="password"
             placeholder="Senha"
           />
-          <Button>Entrar</Button>
+          <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="#">
           <FiLogIn /> Criar Conta
