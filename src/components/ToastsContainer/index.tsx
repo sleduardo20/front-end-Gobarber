b4c00fbb3866addbd66a9e406fc325a135022229
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-05 22:52:46 -0300

    chore: isolading Toast in component

--- src/components/ToastsContainer/index.tsx
+++ src/components/ToastsContainer/index.tsx
@@ -1,34 +1,20 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { ToastMessage, useToast } from '../../hooks/Toast';
 
-import { Container, Toast } from './styles';
+import { ToastMessage } from '../../hooks/Toast';
+
+import Toast from './Toast';
+
+import { Container } from './styles';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastsContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
   return (
     <Container>
       {messages.map(message => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button type="button" onClick={() => removeToast(message.id)}>
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
