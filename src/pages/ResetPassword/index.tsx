1a5b95f03c178de110a729231d0ba76b34dad348
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-07 12:22:49 -0300

    chore: implementation at reset password

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import getValidationErrors from '../../utils/getValidationErros';
 
@@ -15,10 +15,11 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetFormData {
-  email: string;
   password: string;
+  password_confirmation: string;
 }
 
 const ResetPassword: React.FC = () => {
@@ -27,6 +28,7 @@ const ResetPassword: React.FC = () => {
   const { addToast } = useToast();
 
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetFormData) => {
@@ -43,6 +45,15 @@ const ResetPassword: React.FC = () => {
 
         await schema.validate(data, {
           abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+        const token = location.search.replace(`?token=`, ``);
+
+        await api.post('password/reset', {
+          password,
+          password_confirmation,
+          token,
         });
 
         history.push('/');
